c3d69846e3a264e093e5537d51ffdeda2d74a955
EllaNancyFay <ellaschofield0@gmail.com>
2014-10-23 11:33:00 +0100

    Create html and add jquery to change the temperature

---
+++ src/thermostatView.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	var thermostat = new Thermostat;
+	$(".temperature h1").text(thermostat.temperature);
+
+	$(".increase_temperature").on('click', function() {
+		thermostat.increaseTemperature()
+		$(".temperature h1").text(thermostat.temperature);
+	});
+
+	$(".decrease_temperature").on('click', function() {
+		thermostat.decreaseTemperature()
+		$(".temperature h1").text(thermostat.temperature);
+	});
+});
+
