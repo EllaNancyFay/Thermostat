e0da6325aee5e38890683084de4a453b578ac9ea
EllaNancyFay <ellaschofield0@gmail.com>
2014-10-23 14:31:01 +0100

    Thermostat temp changes color depending on what it's set to

--- src/thermostatView.js
+++ src/thermostatView.js
@@ -1,33 +1,36 @@
 $(document).ready(function() {
 	var thermostat = new Thermostat;
-	$(".temperature h1").text(thermostat.temperature);
+	$(".temperature h1").text(thermostat.temperature).css('color', thermostat.displayColour);
 	$(".powerSaveOn").attr('checked', 'checked');
 
 	$(".increase_temperature").on('click', function() {
-		thermostat.increaseTemperature()
-		$(".temperature h1").text(thermostat.temperature);
+		thermostat.increaseTemperature();
+		thermostat.setDisplayColour();
+		$(".temperature h1").text(thermostat.temperature).css('color', thermostat.displayColour);
 	});
 
 	$(".decrease_temperature").on('click', function() {
-		thermostat.decreaseTemperature()
-		$(".temperature h1").text(thermostat.temperature);
+		thermostat.decreaseTemperature();
+		thermostat.setDisplayColour();
+		$(".temperature h1").text(thermostat.temperature).css('color', thermostat.displayColour);
 	});
 
 	$(".reset").on('click', function() {
-		thermostat.resetTemperature()
-		$(".temperature h1").text(thermostat.temperature);
+		thermostat.resetTemperature();
+		thermostat.setDisplayColour();
+		$(".temperature h1").text(thermostat.temperature).css('color', thermostat.displayColour);
 	});
 
 	$(".powerSaveOn").on('click', function() {
 		thermostat.turnOnPowerSaving()
-		$(".temperature h1").text(thermostat.temperature);
+		thermostat.setDisplayColour()
+		$(".temperature h1").text(thermostat.temperature).css('color', thermostat.displayColour);
 	});
 
 	$(".powerSaveOff").on('click', function() {
 		thermostat.turnOffPowerSaving()
 	});
 		
-
 	$.getJSON("http://api.openweathermap.org/data/2.5/find?q=London&units=metric", function(data){
 	$("#actualWeather").text(data['list'][1]['main']['temp'])
 	});
